e2e9754a4a8a6e2f8176aea38e6c2a4a995f1b08
Yifan Yang <yangyifan1@xiaomi.com>
2020-07-05 23:56:55 +0800

    finish: finish lesson 13

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -91,6 +91,7 @@ public class RubyController : MonoBehaviour
             invincibleTimer = timeInvincible;
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
---
+++ Assets/Scripts/UIHealthBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHealthBar : MonoBehaviour
+{
+    public static UIHealthBar instance { get; private set; }
+
+    public Image mask;
+    float originalSize;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        Debug.Log(mask);
+        originalSize = mask.rectTransform.rect.width;
+        Debug.Log(originalSize);
+    }
+
+    public void SetValue(float value)
+    {
+        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
+    }
+}
